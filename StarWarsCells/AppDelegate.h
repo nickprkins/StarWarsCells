7c6b077271d67b073923a6e419a94362398a85fe
Nick Perkins <nickperkins@protonmail.com>
2016-05-04 15:54:26 -0400

    Initial Commit

---
+++ StarWarsCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarWarsCells
+//
+//  Created by Nick Perkins on 5/4/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StarWarsCells/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StarWarsCells
+//
+//  Created by Nick Perkins on 5/4/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
